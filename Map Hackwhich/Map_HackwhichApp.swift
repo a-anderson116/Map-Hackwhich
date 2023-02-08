16e68a18508660d40518c0bf02daf433470a995d
Alex Anderson <andersonaj116@gmail.com>
2023-02-08 09:40:06 -0600

    Initial Commit

---
+++ Map Hackwhich/Map_HackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwhichApp.swift
+//  Map Hackwhich
+//
+//  Created by Alex Anderson on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
